e0244da8f91c6a3b778b5492f119e95cb1860153
Muhammad Paizal <muhammadpaizal43@gmail.com>
2018-12-30 18:33:07 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,16 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload opo kae kae.
+          </p>
+          <a
+            className="App-link"
+            target="_blank"
+            rel="noopener noreferrer"
           >
             Learn React 
           </a>
